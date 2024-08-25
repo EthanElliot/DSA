9399167a142969bb6236e3c07b0050bcbd5b5992
squiddude38 <squiddude38@gmail.com>
2024-08-25 16:47:30 +1200

    added c seqential search

---
+++ C/searching/sequential_search.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sequential_search(int *list, int size, int searchvalue);
+
+int main(void)
+{
+    int size, searchvalue, found;
+    printf("Number of elements in the list: ");
+    scanf("%d", &size);
+
+    int *list = (int *)malloc(sizeof(int) * size);
+
+    printf("Enter %d integers for the list: \n", size);
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &list[i]);
+    }
+
+    printf("What is the value you want to search for: ");
+    scanf("%d", &searchvalue);
+
+    found = sequential_search(list, size, searchvalue);
+    if (found == -1)
+    {
+        printf("Item was not in list\n");
+    }
+    else
+    {
+        printf("Item was found in list at index %d\n", found);
+    }
+}
+
+int sequential_search(int *list, int size, int searchvalue)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (list[i] == searchvalue)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
